44f01f52e9e5faaf00959f80c0c2fec8206485c3
Shan Chenyu <103872353+ischenyu@users.noreply.github.com>
2024-02-18 16:06:14 +0800

    更新 app.py

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ from flask_sqlalchemy import SQLAlchemy
 from models import mysqldb
 
 app = Flask(__name__, static_folder='static')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://python:Dingtalk1234561017@192.168.1.15:3306/python'
 
 @app.route("/")
 def index():
