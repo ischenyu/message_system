5cd0c129a6a295f988ac9d0df539ed6b6d036868
ischenyu <abb1234aabb@gmail.com>
2024-02-27 16:05:01 +0800

    优化逻辑

--- app.py
+++ app.py
@@ -29,21 +29,27 @@ def api():
         print(data)
         email = data['email']
         input_code = data['vercode']
+        # print(email, input_code)
         password = data['password']
         username = data['username']
-        try:
-            if email_captcha.verify_code(email, input_code) == True:
-                if mysqldb.User_signup(username, password, email) == True:
-                    return jsonify({"success":True, "message": "注册成功"})
-                    # ToDo：用户信息写入数据库，注册成功后重定向到登录页面
-            else:
-                return jsonify({"message": "验证码错误"}),400
-        except NameError as e:
-            print(e)
-            return jsonify({"success":False, "message": "验证码错误NameError"})
-        except Exception as e:
-            print(e)
-            return jsonify({"success":False, "message": "验证码错误Exception"})
+        if data['agreement'] == 'on':
+            try:
+                if email_captcha.verify_code(email, input_code) == True:
+                    if mysqldb.User_signup(username, password, email) == True:
+                        return jsonify({"code": 200,"success":True, "message": "注册成功"})
+                        # ToDo：用户信息写入数据库，注册成功后重定向到登录页面
+                    else:
+                        return jsonify({"code":500,"success":False, "message": "注册失败,数据库错误,请联系管理员解决"}),401
+                else:
+                    return jsonify({"message": "验证码错误"}),401
+            except NameError as e:
+                print(e)
+                return jsonify({"success":False, "message": "验证码错误NameError"})
+            except Exception as e:
+                print(e)
+                return jsonify({"success":False, "message": "验证码错误Exception"})
+        else:
+            return jsonify({"success":False, "message": "请同意协议"}),400
     else:
         return "这是api接口,你给我爬啊", 403
     
@@ -85,9 +91,9 @@ def captcha():
             current_time = time.time()
             if current_time - last_request_time[email] < 60:
                 return jsonify({"success": False, 'message': 'Please wait 60 seconds before requesting another captcha'}), 403
-        code = random.randint(100000, 999999)
+        code = random.randint(000000, 999999)
         # 发送验证码到邮箱
-        email_captcha.send(email, code)
+        email_captcha.send_email(email, code)
 
         # 更新用户最近一次请求验证码的时间
         last_request_time[email] = time.time()
